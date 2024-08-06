d288131cb9f5ab6528cf9a4614ca2b7a3b424041
Mohammad Maroof <mmaroof487@gmail.com>
2024-08-06 13:15:37 +0530

    Create Week3_Q3.java

---
+++ Week3_Q3.java
@@ -0,0 +1,37 @@
+class Student {
+    String name;
+    int rollNumber;
+    double marks;
+
+    Student(String name, int rollNumber, double marks) {
+        this.name = name;
+        this.rollNumber = rollNumber;
+        this.marks = marks;
+    }
+
+    char calculateGrade() {
+        if (marks >= 90) {
+            return 'A';
+        } else if (marks >= 80) {
+            return 'B';
+        } else if (marks >= 70) {
+            return 'C';
+        } else if (marks >= 60) {
+            return 'D';
+        } else {
+            return 'F';
+        }
+    }
+
+    void displayStudentInfo() {
+        System.out.println("Name: " + name);
+        System.out.println("Roll Number: " + rollNumber);
+        System.out.println("Marks: " + marks);
+        System.out.println("Grade: " + calculateGrade());
+    }
+
+    public static void main(String[] args) {
+        Student student = new Student("John Doe", 101, 85.5);
+        student.displayStudentInfo();
+    }
+}
